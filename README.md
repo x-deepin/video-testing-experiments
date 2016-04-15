67c8bd363aa40a0af1b2b7426813c21c4f5a5bf0
Sian Cao <yinshuiboy@gmail.com>
2016-04-15 16:00:03 +0800

    Basic xorg config test

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ video-check-scripts
 ===
 Right now, These are only a few testing experiments that finally
 needed to be merged into fixme test cases.
+
+
+thoughts
+===
+- which needs nomodeset? some intel cards, some with nouveau driver.
+how to detect?
+
+
+todos
+===
+- kms api test (this may help to determine if nomodeset needed in kernel cmdline)
+- generate snippet of xorg.conf and do X startup testing?
---
+++ xorg_test.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <string>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+static string run_and_collect(const string& cmd)
+{
+    int pfd[2];
+    pipe(pfd);
+    
+    pid_t pid = fork();
+    if (pid < 0) {
+        return "";
+    }
+
+    if (pid > 0) {
+        string data;
+        close(pfd[1]);
+
+        char buf[129];
+        ssize_t sz = 0;
+        while ((sz = read(pfd[0], &buf, 128)) > 0) {
+            buf[sz] = 0;
+            data += buf;
+        }
+
+        waitpid(pid, NULL, 0);
+        close(pfd[0]);
+        return data;
+
+    } else {
+        close(pfd[0]);
+        dup2(pfd[1], 1);
+
+        execlp(cmd.c_str(), cmd.c_str(), NULL);
+    }
+    return "";
+}
+
+#if 1
+class EnvironmentChecker {
+public:
+	struct VideoEnv {
+		static const int Unknown     = 0;
+		static const int Intel       = 0x0001;
+		static const int AMD         = 0x0002;
+		static const int Nvidia      = 0x0004;
+		static const int VirtualBox  = 0x0100;
+		static const int VMWare      = 0x0200;
+	};
+
+	int doTest() {
+		if (!isDriverLoadedCorrectly()) {
+			return 1;
+		}
+
+
+		_video = VideoEnv::Unknown;
+
+		static regex vbox(".*vga.*virtualbox.*", regex::icase);
+		static regex vmware(".*vga.*vmware.*", regex::icase);
+		static regex intel(".*(vga|3d).*intel.*", regex::icase);
+		static regex amd(".*(vga|3d).*ati.*", regex::icase);
+		static regex nvidia(".*(vga|3d).*nvidia.*", regex::icase);
+
+		string data = run_and_collect("lspci");
+        std::stringstream ss(data);
+        for (string ln; std::getline(ss, ln); ) {
+            if (regex_match(ln, vbox)) {
+                _video |= VideoEnv::VirtualBox;
+            } else if (regex_match(ln, vmware)) {
+                _video |= VideoEnv::VMWare;
+            } else if (regex_match(ln, intel)) {
+                _video |= VideoEnv::Intel;
+            } else if (regex_match(ln, amd)) {
+                _video |= VideoEnv::AMD;
+            } else if (regex_match(ln, nvidia)) {
+                _video |= VideoEnv::Nvidia;
+            }
+        }
+
+		string msg = "video env:";
+		if (_video & VideoEnv::VirtualBox) msg += " VirtualBox";
+		if (_video & VideoEnv::VMWare) msg += " VMWare";
+		if (_video & VideoEnv::Intel) msg += " Intel";
+		if (_video & VideoEnv::AMD) msg += " AMD";
+		if (_video & VideoEnv::Nvidia) msg += " Nvidia";
+        cerr << msg << endl;
+        if (_video == VideoEnv::Unknown) {
+            return 1;
+        }
+
+        //TODO: check lsmod and needed config
+        return 0;
+	}
+
+    //FIXME: this is too weak, need a better way
+	bool isDriverLoadedCorrectly() {
+		static regex tests[] = {
+            //FIXME: actually I think there are some AIGLX error can be omitted.
+            regex(".*(EE)\\s+AIGLX error.*"),
+            regex(".*direct rendering.*disabled.*", regex::icase),
+            regex(".*GLX: Initialized DRISWRAST GL provider.*", regex::icase),
+            //FIXME: DRI v1 is indirect rendering, should not be used.
+            regex(".*direct rendering: DRI enabled.*", regex::icase),
+        };
+
+        std::ifstream f("/var/log/Xorg.0.log");
+		for (string ln; std::getline(f, ln); ) {
+            for (auto& r: tests) {
+                if (regex_match(ln, r)) {
+                    return false;
+                }
+            }
+		}
+
+		return true;
+	}
+
+private:
+	int _video {VideoEnv::Unknown};
+};
+#endif
+
+int main(int argc, char *argv[])
+{
+    EnvironmentChecker checker;
+    return checker.doTest();
+}
+
